d081b17e3b6d94aa9868488b772ca54a0dd8df1b
webdv <abykurian555@gmail.com>
2025-07-24 10:14:00 +0530

    Add files via upload

--- data.java
+++ data.java
@@ -1,1 +1,20 @@
+interface Animal {
+    void makeSound();
+    void eat();
+}
+class Dog implements Animal {
+    public void makeSound() {
+        System.out.println("Dog barks");
+    }
 
+    public void eat() {
+        System.out.println("Dog eats bones");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        myDog.makeSound();
+        myDog.eat();
+    }
+}
